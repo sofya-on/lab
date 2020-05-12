b3af405561770f43ed01ac0bfa4f0428c979a421
sofya-on <47686226+sofya-on@users.noreply.github.com>
2020-05-12 05:52:01 +0300

    Add files via upload

---
+++ Inf.m
@@ -0,0 +1,22 @@
+function res = Inf(p)
+  a = 0;
+  bool = false;
+  bool2 = false;
+  for i = 2:2:numel(p)
+    p(i) = -p(i);
+  endfor
+  while (bool == false)
+    bool = true;
+    a++;
+    copy = p;
+    for i = 2:numel(copy)
+      copy(i) = copy(i-1)*a + copy(i);
+          if (copy(i) < 0)
+            bool = bool*bool2;
+          endif
+    endfor
+    if (bool == true)
+      res = -a;
+    endif
+   endwhile 
+endfunction
---
+++ Sup.m
@@ -0,0 +1,19 @@
+function res = Sup(p)
+  a = 0;
+  bool = false;
+  bool2 = false;
+  while (bool == false)
+    bool = true;
+    a++;
+    copy = p;
+    for i = 2:numel(copy)
+      copy(i) = copy(i-1)*a + copy(i);
+          if (copy(i) < 0)
+            bool = bool*bool2;
+          endif
+    endfor
+    if (bool == true)
+      res = a;
+    endif
+  endwhile
+endfunction
