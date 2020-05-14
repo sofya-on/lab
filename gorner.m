0cdf1c6f6eb5203f0bd1e67315b0e6a893d77a2f
sofya-on <47686226+sofya-on@users.noreply.github.com>
2020-05-14 10:37:32 +0300

    Update gorner.m

--- gorner.m
+++ gorner.m
@@ -1,8 +1,6 @@
-function retval = gorner (p, a)
-  sum=0;
-for i=1:length(p)
-  sum = sum*a;
-  sum = sum + p(i);
-endfor
-  retval = sum;
+function res = Value(p, a)
+  res = p(1);
+  for i = 2:numel(p)
+    res = res*a + p(i);
+  endfor
 endfunction
